6c36b756fd78dccac0ada69b51d9bb70bfc2a8e3
Paul <2907767@alu.murciaeduca.es>
2026-01-15 13:08:04 +0100

    Control de recursos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,7 +7,9 @@ use App\Models\User;
 use Database\Seeders\FamiliasProfesionalesTableSeeder;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,16 +20,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        /* User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]); */
+        Model::unguard();
+        Schema::disableForeignKeyConstraints();
 
         self::seedProyectos();
         $this->command->info('Tabla catálogo inicializada con datos!');
         $this->call(FamiliasProfesionalesTableSeeder::class);
+        $this->call(CiclosTableSeeder::class);
+
+        Model::reguard();
+        Schema::enableForeignKeyConstraints();
     }
 
     private function seedProyectos():void
